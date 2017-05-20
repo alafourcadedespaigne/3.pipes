54ba72119d91c9aff871d4c25c53d143a188d634
Alejandro Lafourcade <alafourcadedespaigne@gmail.com>
2017-05-20 16:39:59 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,33 @@ export class AppComponent {
 
   nombre = "Alejandro"
 
-  arreglo=[1,2,3,4,5,6,7,8,9,10];
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   PI = Math.PI;
 
   a = 0.234;
 
+  salario = 1234.5;
+
+  heroe = {
+
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+
+    }
+  }
+
+  valorDePromesa = new Promise((resolve, reject)=> {
+
+    setTimeout(()=>resolve("Llego la data"), 3500);
+  })
+
+  nombre2 = "aLejanANDRo lAFOURCADE despaiGne";
+
+  activar:boolean = true;
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {CapitalizadoPipe} from "./pipes/capitalizado.pipe";
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule,
